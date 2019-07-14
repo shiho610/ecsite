ff66df0d163de666ba9b563411e2cd5c75873124
EC2 Default User <ec2-user@ip-172-31-9-186.us-east-2.compute.internal>
2019-07-14 06:23:01 +0000

    second

---
+++ include/conf/const.php
@@ -0,0 +1,9 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWD', '9G9WguXVckNJnCrA');
+define('DB_NAME', 'ecsite');
+ 
+define('HTML_CHARACTER_SET', 'UTF-8');  // HTML文字エンコーディング
+define('DB_CHARACTER_SET',   'UTF8');   // DB文字エンコーディング
