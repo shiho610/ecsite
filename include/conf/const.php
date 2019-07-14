98d6df83eb92866fda36f9eabe91303e266d12bd
shiho610 <52369628+shiho610@users.noreply.github.com>
2019-07-14 21:14:53 +0900

    Update const.php

--- include/conf/const.php
+++ include/conf/const.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_HOST', 'us-cdbr-iron-east-02.cleardb.net');
-define('DB_USER', 'be7bc579bec5cb');
-define('DB_PASSWD', '52e10e23');
-define('DB_NAME', 'heroku_bde924a0558fa07');
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWD', '9G9WguXVckNJnCrA');
+define('DB_NAME', 'ecsite');
  
 define('HTML_CHARACTER_SET', 'UTF-8');  // HTML文字エンコーディング
 define('DB_CHARACTER_SET',   'UTF8');   // DB文字エンコーディング
