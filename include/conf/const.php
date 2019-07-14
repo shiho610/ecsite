af27490bfd4c6fd15f750949174955be7c8be118
EC2 Default User <ec2-user@ip-172-31-9-186.us-east-2.compute.internal>
2019-07-14 15:30:20 +0000

    changelast

--- include/conf/const.php
+++ include/conf/const.php
@@ -1,9 +1,11 @@
 <?php
 
-define('DB_HOST', 'us-cdbr-iron-east-02.cleardb.net');
-define('DB_USER', 'be7bc579bec5cb');
-define('DB_PASSWD', '52e10e23');
-define('DB_NAME', 'heroku_bde924a0558fa07');
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+define('DB_HOST', $url['us-cdbr-iron-east-02.cleardb.net']);
+define('DB_USER', $url['be7bc579bec5cb']);
+define('DB_PASSWD', $url['52e10e23']);
+define('DB_NAME', substr($url['heroku_bde924a0558fa07'], 1));
  
 define('HTML_CHARACTER_SET', 'UTF-8');  // HTML文字エンコーディング
 define('DB_CHARACTER_SET',   'UTF8');   // DB文字エンコーディング
