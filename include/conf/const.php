b6c531cdc72e3a740895fab8d8a7d8f0af1e4313
EC2 Default User <ec2-user@ip-172-31-9-186.us-east-2.compute.internal>
2019-07-15 01:53:11 +0000

    mmm

--- include/conf/const.php
+++ include/conf/const.php
@@ -1,13 +1,12 @@
 <?php
 
-/*$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+define('DB_HOST', 'us-cdbr-iron-east-02.cleardb.net');
+define('DB_USER', 'be7bc579bec5cb');
+define('DB_PASSWD', '52e10e23');
+define('DB_NAME', 'heroku_bde924a0558fa07');
 
-define('DB_HOST', $url['us-cdbr-iron-east-02.cleardb.net']);
-define('DB_USER', $url['be7bc579bec5cb']);
-define('DB_PASSWD', $url['52e10e23']);
-define('DB_NAME', substr($url['heroku_bde924a0558fa07'], 1));
-*/
 
+/*
 $db = parse_url($_SERVER['CLEARDB_DATABASE_URL']);
 $db['heroku_bde924a0558fa07'] = ltrim($db['path'], '/');
 $dsn = "mysql:host={$db['us-cdbr-iron-east-02.cleardb.net']};dbname={$db['heroku_bde924a0558fa07']};charset=utf8";
@@ -18,6 +17,7 @@ $options = array(
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::MYSQL_ATTR_USE_BUFFERED_QUERY =>true,
   );
+*/
  
 define('HTML_CHARACTER_SET', 'UTF-8');  // HTML文字エンコーディング
 define('DB_CHARACTER_SET',   'UTF8');   // DB文字エンコーディング
